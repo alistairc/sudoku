596f0a6cf8fb90abb2686673cd35336ae8fd2472
Alistair Cooke <alistair.cooke77@gmail.com>
2022-03-12 18:54:32 +0000

    Use Enums for the indices

--- src/Sudoku/Grid.hs
+++ src/Sudoku/Grid.hs
@@ -25,10 +25,10 @@ newtype Grid = Grid [Maybe Digit]
   deriving (Show, Eq)
 
 data Column = C1 | C2 | C3 | C4 | C5 | C6 | C7 | C8 | C9
-  deriving (Show, Eq, Ord)
+  deriving (Show, Eq, Ord, Enum)
 
 data Row = R1 | R2 | R3 | R4 | R5 | R6 | R7 | R8 | R9
-  deriving (Show, Eq, Ord)
+  deriving (Show, Eq, Ord, Enum)
 
 newtype DigitSet = DigitSet [Maybe Digit]
   deriving (Show, Eq)
@@ -42,9 +42,9 @@ emptyDigitSet :: DigitSet
 emptyDigitSet = DigitSet (replicate 9 Nothing)
 
 listToGrid :: [Maybe Digit] -> Maybe Grid
-listToGrid list = 
-  if length list == 81 
-  then Just (Grid list) 
+listToGrid list =
+  if length list == 81
+  then Just (Grid list)
   else Nothing
 
 gridToList :: Grid -> [Maybe Digit]
@@ -53,38 +53,20 @@ gridToList (Grid squares) = squares
 moveAt :: GridCoord -> Digit -> Grid -> Grid
 moveAt (x, y) digit initial =
   let (Grid squares) = initial
-      index = gridIndex (x,y) 
+      index = gridIndex (x,y)
    in Grid $ take index squares ++ [Just digit] ++ drop (index + 1) squares
 
 getSquare :: GridCoord -> Grid -> Maybe Digit
 getSquare (x,y) grid = gridToList grid !! gridIndex (x,y)
 
 gridIndex :: GridCoord -> Int
-gridIndex (x,y) = (rowIndex y - 1) * 9 + (columIndex x - 1)
+gridIndex (x,y) = (rowIndex y - 1) * 9 + (columnIndex x - 1)
 
-columIndex :: Column -> Int
-columIndex col = case col of
-  C1 -> 1
-  C2 -> 2
-  C3 -> 3
-  C4 -> 4
-  C5 -> 5
-  C6 -> 6
-  C7 -> 7
-  C8 -> 8
-  C9 -> 9
+columnIndex :: Column -> Int
+columnIndex col = fromEnum col + 1
 
 rowIndex :: Row -> Int
-rowIndex row = case row of
-  R1 -> 1
-  R2 -> 2
-  R3 -> 3
-  R4 -> 4
-  R5 -> 5
-  R6 -> 6
-  R7 -> 7
-  R8 -> 8
-  R9 -> 9
+rowIndex row = fromEnum row + 1
 
 selectRow :: Row -> Grid -> DigitSet
 selectRow row (Grid digits) =
