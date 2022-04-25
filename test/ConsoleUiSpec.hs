ae048c9d8de082ac30c62154c8b784c63668ea04
Alistair Cooke <alistair.cooke77@gmail.com>
2022-04-25 20:24:31 +0100

    Monad tests can access the return value
    Not just teh state of the console.  Will allow testing more types of code
    I think

--- test/ConsoleUiSpec.hs
+++ test/ConsoleUiSpec.hs
@@ -10,9 +10,15 @@ spec :: Spec
 spec =
   describe "on startup" $
     it "should display an empty grid" $
-      let consoleOutput = runTestConsoleApp runSudoku
-       in lastOutput consoleOutput `shouldBe` renderGrid emptyGrid
+      let
+        consoleOutput = runTestConsoleApp $ do
+          runSudoku
+          getConsoleLines
+       in
+        lastOutput consoleOutput `shouldBe` renderGrid emptyGrid
 
+getConsoleLines :: TestConsoleApp [String]
+getConsoleLines = get
 
 lastOutput :: [String] -> String
 lastOutput [] = "" -- to avoid the partial
@@ -23,8 +29,9 @@ newtype TestConsoleApp a = TestConsoleApp (State [String] a)
   deriving (Functor, Applicative, Monad, MonadState [String])
 
 -- run function for our custom monad.
-runTestConsoleApp :: TestConsoleApp a -> [String]
-runTestConsoleApp (TestConsoleApp stdOutState) = execState stdOutState []
+runTestConsoleApp :: TestConsoleApp a -> a
+runTestConsoleApp (TestConsoleApp stdOutState) =
+  evalState stdOutState []
 
 instance MonadConsole TestConsoleApp where
   consoleWrite text = modify (text :) -- not a smiley! :) add test to head of list
